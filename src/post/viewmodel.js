73d577e47b3594c3c72c22cf46b1ddaa44faeca8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-03 20:25:19 +0000

    Deploy scriptype/karicca.me to github.com/scriptype/karicca.me.git:gh-pages

--- src/post/viewmodel.js
+++ src/post/viewmodel.js
@@ -1,5 +1,6 @@
-import { LoadingPostView, PostView } from './view'
+import { LoadingPostView, PostView, FullImageView } from './view'
 import { PostModel } from './model'
+import { toDOM } from '../utils'
 import router from '../router'
 
 const model = new PostModel()
@@ -14,6 +15,26 @@ const addEventListeners = () => {
     } else {
       router.navigate('', { trigger: true })
     }
+  })
+
+  const imagesQuery = 'a[href*="tumblr"] img[src*="tumblr"]'
+  const images = container.querySelectorAll(imagesQuery)
+  images.forEach(image => {
+    image.addEventListener('click', e => {
+      e.preventDefault()
+      e.stopPropagation()
+      showFullImage(image)
+    })
+  })
+}
+
+const showFullImage = (image) => {
+  container.classList.add('is-showing-larger-image')
+  const fullImageContainer = toDOM(FullImageView(image))
+  container.appendChild(fullImageContainer)
+  fullImageContainer.addEventListener('click', e => {
+    container.classList.remove('is-showing-larger-image')
+    fullImageContainer.remove()
   })
 }
 
