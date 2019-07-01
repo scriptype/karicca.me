f3f682a1ea79935d40879ed1ed5c9979b92ea4d2
scriptype <scriptyper@gmail.com>
2019-07-02 01:31:34 +0300

    Move all static assets under src/assets folder

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ export default {
     apiKey: 'rPSt5BHEMqYFbAR6UVccYzEiLXWw6CSE92RTWbz9QOUim6W7TQ'
   },
   parallaxLayers: [
-    '/src/assets/layer1.jpg',
-    '/src/assets/layer2.png',
-    '/src/assets/layer3.png',
-    '/src/assets/layer4.png',
-    '/src/assets/layer5.png'
+    '/src/assets/parallax/layer1.jpg',
+    '/src/assets/parallax/layer2.png',
+    '/src/assets/parallax/layer3.png',
+    '/src/assets/parallax/layer4.png',
+    '/src/assets/parallax/layer5.png'
   ]
 }
