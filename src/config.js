aba3edf8592a1eb169355b99e191840165b4c645
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-02 00:15:38 +0000

    Deploy scriptype/karicca.me to github.com/scriptype/karicca.me.git:gh-pages

--- src/config.js
+++ src/config.js
@@ -9,5 +9,19 @@ export default {
     '/src/assets/parallax/layer3.png',
     '/src/assets/parallax/layer4.png',
     '/src/assets/parallax/layer5.png'
+  ],
+  socialMedia: [
+    {
+      text: 'Instagram',
+      href: 'https://www.instagram.com/karicca'
+    },
+    {
+      text: 'Behance',
+      href: 'https://www.behance.net/karicca'
+    },
+    {
+      text: 'Twitter',
+      href: 'https://twitter.com/jellymedyza'
+    }
   ]
 }
