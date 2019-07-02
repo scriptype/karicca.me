320d7b0ea0d1e3f8bcfb13cb5e5c7d44dcabbe66
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-02 23:11:53 +0000

    Deploy scriptype/karicca.me to github.com/scriptype/karicca.me.git:gh-pages

--- src/components/navigation/view.js
+++ src/components/navigation/view.js
@@ -7,14 +7,14 @@ export function NavigationView(items) {
         class="navigation__btn navigation__open-btn"
         id="open-navigation-btn"
       >
-        <svg class="navigation__svg" version="1.1" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" xmlns:xlink="http://www.w3.org/1999/xlink"><title/><desc/><defs/><g fill="currentColor" id="Hamburger"><path d="M0,2 L25,2 L25,6 L0,6 L0,2 Z M0,10 L25,10 L25,14 L0,14 L0,10 Z M0,18 L25,18 L25,22 L0,22 L0,18 Z"/></g></svg>
+        <svg class="navigation__icon" version="1.1" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" xmlns:xlink="http://www.w3.org/1999/xlink"><title/><desc/><defs/><g fill="currentColor" id="Hamburger"><path d="M0,2 L25,2 L25,6 L0,6 L0,2 Z M0,10 L25,10 L25,14 L0,14 L0,10 Z M0,18 L25,18 L25,22 L0,22 L0,18 Z"/></g></svg>
       </button>
       <button
         type="button"
         class="navigation__btn navigation__close-btn"
         id="close-navigation-btn"
       >
-        <span class="close-icon"></span>
+        <span class="navigation__icon close-icon"></span>
       </button>
       <ul class="navigation">
         ${items.map(({ text, href, id }) => `
